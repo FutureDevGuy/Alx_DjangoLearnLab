b76fc42e45e68b42a0b546db6058c5b5a3d3ae53
Philip Oseikhuemen Inegbedion <philliposeikhuemen@gmail.com>
2025-09-07 23:35:23 +0100

    Add registration URL pattern in relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,10 +3,7 @@ from .views import list_books, LibraryDetailView, RegisterView
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # Books listing (function-based view)
     path('books/', list_books, name='list_books'),
-
-    # Library detail (class-based view)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs
